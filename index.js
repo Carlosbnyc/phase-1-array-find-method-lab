a9b578ee0cbf4b6312659a3bc5bb139d4020abb7
Carlos <bowyercarlos@gmail.com>
2023-11-18 18:47:58 -0500

    superbowl win function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin (record){
+    let obj =  record.find(obj=>{
+        if (obj.result === "W") {
+            return (obj)
+        }
+    })
+
+    if (obj){
+        return (obj.year)
+    }
+}
